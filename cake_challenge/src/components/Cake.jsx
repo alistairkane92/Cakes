7b5ce6709912e3d64a6f38ef2f9712f4fa4e1f2d
Alistair Kane <vplelmo@hotmail.co.uk>
2018-01-15 16:42:23 +0000

    adds basic CSS

--- cake_challenge/src/components/Cake.jsx
+++ cake_challenge/src/components/Cake.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Cake = (props) => {
   return(
-    <div>
-      <img className="cake-img" key={props.cake.index} value={props.cakeindex} src={props.cake.imageUrl} alt="cake-image"/>
+    <div className="cake-div">
+      <img className="cake-img" key={props.cake.index} value={props.cake.index} src={props.cake.imageUrl} alt="cake-image"/>
       <h3 className="cake-name">{props.cake.name}</h3>
     </div>
   )
